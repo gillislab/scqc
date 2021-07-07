c61bb7b216ae93318754f1e29f8fdddd16afdbbb
John Lee <johlee@cshl.edu>
2021-07-06 22:39:07 -0400

    dataset specific figures

---
+++ scqc/buildfigures.py
@@ -0,0 +1,269 @@
+# aggregates statistics.
+
+import argparse
+import io
+import logging
+import os
+import subprocess
+import sys
+from configparser import ConfigParser
+from queue import Empty, Queue
+from threading import Thread
+import glob
+import numpy as np
+import pandas as pd
+import scanpy as sc  # pip install
+from scipy.io import mmread
+from scipy.sparse import base
+import matplotlib.pyplot as plt
+import seaborn as sns       # inputs as dataframes
+# import matplotlib.cbook as cbook
+
+plt.rcParams.update({'figure.autolayout': True})
+
+sns.set_style("ticks")
+sns.set_context("paper")
+sns.despine()
+
+gitpath = os.path.expanduser("~/git/scqc")
+sys.path.append(gitpath)
+from scqc.utils import *
+
+LOGLEVELS = {
+    10: 'debug',
+    20: 'info',
+    30: 'warn',
+    40: 'err',
+    50: 'fatal',
+}
+
+class BuildFigures(object):
+    def __init__(self,config):
+        self.log = logging.getLogger('figures')
+        self.config = config
+        self.figuredir = os.path.expanduser(    # where to store figures
+            self.config.get('figures', 'figuredir'))
+        self.outputdir = os.path.expanduser(    # where the adata is stored
+            self.config.get('figures', 'outputdir'))
+
+    def execute(self, srpid):
+        # get the srpid
+
+        # get the h5ad paths for the project
+        h5paths = f'{self.outputdir}/{srpid}'
+        h5paths = glob.glob(f'{h5paths}/*h5ad')
+
+        try:
+            os.makedirs(f'{self.figuredir}/{srpid}')
+        except FileExistsError:
+            pass
+
+        for h5path in h5paths:
+            basename = os.path.basename(h5path)
+            rootname = os.path.dirname(h5path)
+
+            adata = sc.read_h5ad(h5path)
+
+            # plot var x against var y
+            figurepath = basename.replace('.h5ad', '_Gender.png')
+            plot_scatter(data=adata.obs, x_column='pct_counts_male', y_column='pct_counts_female',  figsize=(4,3),
+                        xlab='Pct Male Markers', ylab=' Pct Male Markers', 
+                        color_by='pct_counts_male',cbar_title='Pct Male Markers',
+                        regression_line=False, hlines = [], vlines =[], 
+                        save_it=True, outfile=f'{self.figuredir}/{srpid}/{figurepath}')
+            
+            # plot on umap 
+            umapdf = pd.DataFrame({
+                    'x': adata.obsm['X_umap'][:,0],
+                    'y':adata.obsm['X_umap'][:,1], 
+                    'Corr_to_Mean':adata.obs['corr_to_mean'],
+                    'Mito':adata.obs['log1p_total_counts_mt'],
+                    # 'Batch':adata.obs['batch'],
+                    'Cell_Cycle':adata.obs['log1p_total_counts_cell_cycle'],
+                    'Essential':adata.obs['log1p_total_counts_essential'],
+                    'Ribosome':adata.obs['log1p_total_counts_ribo']
+                    })
+            for kw in umapdf.columns[2:] :
+                figurepath = basename.replace('.h5ad', f'_umap_{kw}.png')
+                plot_scatter(data=umapdf, x_column='x', y_column='y',  figsize=(4,3),
+                            xlab='UMAP_1', ylab=' UMAP_2', 
+                            color_by=kw,cbar_title=kw,
+                            regression_line=False, hlines = [], vlines =[], 
+                            save_it=True, outfile=f'{self.figuredir}/{srpid}/{figurepath}')
+
+
+            # plot histograms 
+            figurepath = basename.replace('.h5ad',f'_gini.png')
+            plot_histogram(adata.obs.gini    , xlab='Gini coefficient', ylab='Frequency', title=None, save_it=True, outfile= f'{figuredir}/{srpid}/{figurepath}',
+                  bins=None, orientation='vertical', vlines=['mean'], fig_size=(4, 3), rotate_xlabel=False, alpha=0.75)
+            figurepath = basename.replace('.h5ad',f'_Corr_to_Mean.png')
+            plot_histogram(adata.obs.corr_to_mean    , xlab='Correlation to Mean', ylab='Frequency', title=None, save_it=True, outfile= f'{figuredir}/{srpid}/{figurepath}',
+                  bins=None, orientation='vertical', vlines=['mean'], fig_size=(4, 3), rotate_xlabel=False, alpha=0.75)
+
+            
+def plot_scatter(data, x_column, y_column, xlab=None, ylab=None, title =None, figsize=(4,3), 
+                marker='.',markersize = 1, color_by=None, cbar_title=None,
+                regression_line=True,  hlines = [], vlines =[],
+                save_it=False, outfile="/home/johlee/scqc/figures/temp_sns.png"):
+
+    colors = None
+    cmap =None
+    fig, ax = plt.subplots(figsize=figsize)
+
+    if color_by  is not None :
+        colors = data[color_by]
+        cmap = sns.color_palette("mako_r", as_cmap=True)
+
+        
+
+    scat = ax.scatter(x=data[x_column], y=data[y_column],  c= colors, marker=marker,s = markersize, cmap = cmap)
+
+    if color_by  is not None :
+        cb = plt.colorbar(scat)
+        cb.set_label(cbar_title)
+
+    scat = ax.set(xlabel=xlab, ylabel=ylab,
+           title=title)
+
+    if regression_line :    # add a regression line
+        m, b = np.polyfit(data[x_column], data[y_column], 1)
+        plt.plot(data[x_column],data[x_column]*m+b, 'k--' ,linewidth=markersize-.5)
+        # sns.regplot(,y,ci=None)
+
+    for vline in list(vlines):
+        if vline == 'mean':
+            m = x.mean()
+            ax.text(m, 0, 'Mean', fontsize=10, horizontalalignment='center')
+            ax.axvline(m, ls='--', color='r',linewidth=markersize-.5)
+        elif vline == 'median':
+            m = x.median()
+            ax.text(m, 0, 'Median', fontsize=10, horizontalalignment='center')
+            ax.axvline(m, ls='--', color='r',linewidth=markersize-.5)
+        elif type(vline) == int or type(vline) == float:
+            ax.axvline(float(vline), ls='--', color='r',linewidth=markersize-.5)
+
+
+    for hline in list(hlines):
+        if hline == 'mean':
+            m = y.mean()
+            ax.text(m, 0, 'Mean', fontsize=10)
+            ax.axyline(m, ls='--', color='r',linewidth=markersize-.5)
+        elif vline == 'median':
+            m = x.median()
+            ax.text(m, 0, 'Median', fontsize=10)
+            ax.axhline(m, ls='--', color='r',linewidth=markersize-.5)
+        elif type(hline) == int or type(vline) == float:
+            ax.axhline(float(vline), ls='--', color='r',linewidth=markersize-.5)
+
+    if save_it:
+        # transparent should be set to true for publication etc. False for testing.
+        fig.savefig(outfile, dpi=300, transparent=False)
+
+    return( fig, ax )
+
+def plot_histogram(x, xlab=None, ylab=None, title=None, save_it=False, outfile='/home/johlee/scqc/figures/temp_hist.png',
+                  bins=None, orientation='vertical', vlines=['mean', 'median'], fig_size=(8, 6), rotate_xlabel=False, alpha=0.75):
+    fig, ax = plt.subplots(figsize=figsize)
+
+    ax.hist(x, bins=bins, orientation=orientation)
+    ax.set(xlabel=xlab, ylabel=ylab,
+           title=title)
+
+    for vline in list(vlines):
+
+        if vline == 'mean':
+            m = x.mean()
+            ax.text(m, 0, 'Mean', fontsize=10, horizontalalignment='center')
+            ax.axvline(m, ls='--', color='r')
+        elif vline == 'median':
+            m = x.median()
+            ax.text(m, 0, 'Median', fontsize=10, horizontalalignment='center')
+            ax.axvline(m, ls='--', color='r')
+        elif type(vline) == int or type(vline) == float:
+            ax.axvline(float(vline), ls='--', color='r')
+
+    for vline in list(vlines):
+
+        if vline == 'mean':
+            m = x.mean()
+            ax.text(m, 0, 'Mean', fontsize=10, horizontalalignment='center')
+            ax.axvline(m, ls='--', color='r')
+        elif vline == 'median':
+            m = x.median()
+            ax.text(m, 0, 'Median', fontsize=10, horizontalalignment='center')
+            ax.axvline(m, ls='--', color='r')
+        elif type(vline) == int or type(vline) == float:
+            ax.axvline(float(vline), ls='--', color='r')
+
+    if rotate_xlabel:
+        labels = ax.get_xticklabels()
+        plt.setp(labels, rotation=45, horizontalalignment='center')
+
+    if save_it:
+        # transparent should be set to true for publication etc. False for testing.
+        fig.savefig(outfile, dpi=300, transparent=False)
+
+    return(fig, ax)
+
+def plot_boxplots(data): 
+    # primarily for cell cycle phases
+
+    # 
+    pass
+
+if __name__ == "__main__":
+
+    FORMAT = '%(asctime)s (UTC) [ %(levelname)s ] %(filename)s:%(lineno)d %(name)s.%(funcName)s(): %(message)s'
+    logging.basicConfig(format=FORMAT)
+    # logging.getLogger().setLevel(logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-d', '--debug',
+                        action="store_true",
+                        dest='debug',
+                        help='debug logging')
+
+    parser.add_argument('-v', '--verbose',
+                        action="store_true",
+                        dest='verbose',
+                        help='verbose logging')
+
+    parser.add_argument('-c', '--config',
+                        action="store",
+                        dest='conffile',
+                        default='~/git/scqc/etc/scqc.conf',
+                        help='Config file path [~/git/scqc/etc/scqc.conf]')
+
+    parser.add_argument('-p', '--projectids',
+                        metavar='projectids',
+                        type=str,
+                        nargs='+',
+                        default=None,
+                        help='gather stats')
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+    if args.verbose:
+        logging.getLogger().setLevel(logging.INFO)
+
+    if args.conffile is not None:
+        cp = ConfigParser()
+        cp.read(os.path.expanduser(args.conffile))
+    else:
+        cp = get_default_config()
+
+    cs = get_configstr(cp)
+    logging.debug(f"got config: {cs}")
+
+    logging.debug(f"args: {args}")
+
+
+
+    if args.projectids is not None:
+        q = BuildFigures(cp)
+        for pid in args.projectids:
+            # args.projectid is a list of project ids
+            q.execute(pid)
--- scqc/gatherstats.py
+++ scqc/gatherstats.py
@@ -9,12 +9,16 @@ import sys
 from configparser import ConfigParser
 from queue import Empty, Queue
 from threading import Thread
-import glob 
+import glob
 import numpy as np
 import pandas as pd
 import scanpy as sc  # pip install
 from scipy.io import mmread
 from scipy.sparse import base
+import matplotlib.pyplot as plt
+import seaborn as sns       # inputs as dataframes
+# import matplotlib.cbook as cbook
+
 
 
 gitpath = os.path.expanduser("~/git/scqc")
@@ -29,84 +33,69 @@ LOGLEVELS = {
     50: 'fatal',
 }
 
-TO_PLOT = ['reads']
+# TODO given just proj_id, split to smart seq and 10x and proceed independently.
+# TODO generate figures for the dataset
 
-#TODO given just proj_id, split to smart seq and 10x and proceed independently.
-#TODO generate figures for the dataset
+
 class GetStats(object):
 
-    def __init__(self, config ):
+    def __init__(self, config):
         self.log = logging.getLogger('statistics')
         self.config = config
         self.outputdir = os.path.expanduser(
             self.config.get('statistics', 'outputdir'))
         self.resourcedir = os.path.expanduser(
             self.config.get('statistics', 'resourcedir'))
-        self.metadir = os.path.expanduser(self.config.get('statistics', 'metadir'))
-        self.tempdir = os.path.expanduser(self.config.get('statistics', 'tempdir'))
+        self.metadir = os.path.expanduser(
+            self.config.get('statistics', 'metadir'))
+        self.tempdir = os.path.expanduser(
+            self.config.get('statistics', 'tempdir'))
 
         # gene sets
-        self.cell_cycle_genes = os.path.expanduser(self.config.get('statistics', 'cell_cycle_genes'))
-        self.stable_housekeepinig = os.path.expanduser(self.config.get('statistics', 'stable_housekeepinig'))
-        self.essential_genes = os.path.expanduser(self.config.get('statistics', 'essential_genes'))
-        self.female_genes = os.path.expanduser(self.config.get('statistics', 'female_genes'))
-        self.male_genes = os.path.expanduser(self.config.get('statistics', 'male_genes'))
+        self.cell_cycle_genes = os.path.expanduser(
+            self.config.get('statistics', 'cell_cycle_genes'))
+        self.stable_housekeepinig = os.path.expanduser(
+            self.config.get('statistics', 'stable_housekeepinig'))
+        self.essential_genes = os.path.expanduser(
+            self.config.get('statistics', 'essential_genes'))
+        self.female_genes = os.path.expanduser(
+            self.config.get('statistics', 'female_genes'))
+        self.male_genes = os.path.expanduser(
+            self.config.get('statistics', 'male_genes'))
 
         self.nPCA = self.config.get('statistics', 'nPCA')
 
-    def execute(self,srpid):
+    def execute(self, srpid):
         # outdir = "/home/johlee/scqc/stats"
         solooutdirs = f'{self.outputdir}/{srpid}'
         solooutdirs = glob.glob(f'{solooutdirs}/*Solo.out')
 
         self.log.debug(f'starting with projectid {srpid}')
 
-        adata ={}
-        for solooutdir in solooutdirs :
+        adata = {}   # loops through all star runs in the project
+        for solooutdir in solooutdirs:
+
             self.log.debug(f'looking at {solooutdir}')
             # solooutdir = "/home/johlee/scqc/starout/SRP308826_smartseq_Solo.out"
-
-            # TODO fix this!  
-            # Dumps stats from STAR into one file and stores done list
-
-            # barcode_stats, feature_stats, summary_stats = self._gather_stats_from_STAR(
-            #     solooutdir)
-            # all_stats = summary_stats.append(barcode_stats).append(feature_stats)
-            # # did we already save these?
-            # acc = barcode_stats.accession.unique()[0]
-            # fname = f'{self.metadata}/starout_stats.tsv'
-            # self.log.debug(f'read {fname}')
-
-            # runs_done = pd.read_csv(
-            #     f'{solooutdir}/Gene/raw/barcodes.tsv', sep="\t", header=None)
-            # projectout = f'{self.outputdir}/{acc}_runs_done.tsv'
-
-            # all_stats.to_csv(fname, sep="\t", index=None,
-            #                 header=not os.path.isfile(fname), mode='w')
-            # runs_done.to_csv(projectout, sep="\t", index=None,
-            #                 header=False, mode='a')
 
             adata[solooutdir] = self._parse_STAR_mtx(solooutdir)
             adata[solooutdir] = self._get_stats_scanpy(adata[solooutdir])
 
             # scanpy default is to overwrite existing file
             basename = os.path.basename(solooutdir)
-            basename =basename.replace('_Solo.out' ,'.h5ad')
+            basename = basename.replace('_Solo.out', '.h5ad')
             h5file = f'{self.outputdir}/{srpid}/{basename}'
             adata[solooutdir].var.index.name = None
             self.log.debug(adata[solooutdir].obs.columns)
             self.log.debug(adata[solooutdir].var.columns)
-            
 
             # for testing...
-            adata[solooutdir].obs.to_csv(f'{self.tempdir}/obs.tsv',sep="\t")
-            adata[solooutdir].var.to_csv(f'{self.tempdir}/var.tsv',sep="\t")
-            # consolidate these... 
+            # adata[solooutdir].obs.to_csv(f'{self.tempdir}/obs.tsv',sep="\t")
+            # adata[solooutdir].var.to_csv(f'{self.tempdir}/var.tsv',sep="\t")
+            # consolidate these...
             adata[solooutdir].write(h5file)
 
-
-
-    def _gather_stats_from_STAR(self,solooutdir):
+    def _gather_stats_from_STAR(self, solooutdir):
 
         with open(f"{solooutdir}/Barcodes.stats", 'r') as f:
             lines = f.readlines()
@@ -136,7 +125,7 @@ class GetStats(object):
 
     # need to pass in star index directory
 
-    def _parse_STAR_mtx(self,solooutdir):
+    def _parse_STAR_mtx(self, solooutdir):
         # note that scanpy uses cell x gene.
         # read into anndata
         # path should end with "Solo.out"
@@ -173,16 +162,15 @@ class GetStats(object):
     def _get_stats_scanpy(self, adata):
         adata.obs['batch'] = None
 
-
         # consider different gene sets - ERCC  corresponds to spike ins
         adata.var['mt'] = adata.var.gene_symbol.str.startswith('mt-')
         # adata.var['ERCC'] = adata.var.gene_symbol.str.startswith('ERCC')
         adata.var['ribo'] = adata.var.type == "rRNA"
-        adata.var['cytoplasm'] = None       # GO Term/kegg?
-        adata.var['metabolism'] = None      # GO Term/kegg?
-        adata.var['membrane'] = None        # GO Term/kegg?
+        # adata.var['cytoplasm'] = None       # GO Term/kegg?
+        # adata.var['metabolism'] = None      # GO Term/kegg?
+        # adata.var['membrane'] = None        # GO Term/kegg?
 
-        qcvars = ['mt','ribo', 'female', 'male',
+        qcvars = ['mt', 'ribo', 'female', 'male',
                   'essential', 'cell_cycle']
 
         # get housekeeping genes
@@ -191,7 +179,6 @@ class GetStats(object):
         self.log.debug(f"housekeeping:{hk_genes}")
         adata.var['housekeeping'] = adata.var.gene_symbol.isin(
             hk_genes.gene)
-
 
         # get gender markers
         female_genes = pd.read_csv(self.female_genes, sep=",")
@@ -206,7 +193,7 @@ class GetStats(object):
 
         # get essential genes
         essential = pd.read_csv(self.essential_genes, sep=",")
-        self.log.debug(f"ess:{essential}" )
+        self.log.debug(f"ess:{essential}")
         adata.var['essential'] = adata.var.gene_symbol.isin(
             essential.gene)
 
@@ -233,46 +220,38 @@ class GetStats(object):
         # this part is slow (~10-15 min) because of a for loop - can parallelize
         self.log.debug('calculating gini for each cell')
         # NOTE SLOW commented for testing
-        adata.obs['gini'] = gini_coefficient_spmat(adata.X )
+        adata.obs['gini'] = gini_coefficient_spmat(adata.X)
 
         # unstructured data - dataset specific
         adata.uns['gini_by_counts'] = gini_coefficient(
             adata.obs['total_counts'])
 
         # highly variable genes - expects log data
-        sc.pp.log1p(adata) # natural log
-        sc.pp.highly_variable_genes( adata,
-                min_mean=self.hvg_min_mean,
-                max_mean=self.hvg_max_mean,
-                min_disp=self.hvg_min_disp,
-                max_disp=self.hvg_max_disp,
-                flavor=self.hvg_flavor)
+        sc.pp.log1p(adata)  # natural log
+        sc.pp.highly_variable_genes(adata,
+                                    min_mean=self.hvg_min_mean,
+                                    max_mean=self.hvg_max_mean,
+                                    min_disp=self.hvg_min_disp,
+                                    max_disp=self.hvg_max_disp,
+                                    flavor=self.hvg_flavor)
 
-
-        # XXX  does it make sense to have a umap for every run? Or one for the project? 
+        # XXX  does it make sense to have a umap for every run? Or one for the project?
         # we'll do both for now
         # umap coords
         sc.tl.pca(adata,
-             n_comps = self.nPCA, zero_center=False,use_highly_variable=self.use_hvg)
-        
+                  n_comps=self.nPCA, zero_center=False, use_highly_variable=self.use_hvg)
+
         sc.pp.neighbors(adata, n_neighbors=self.n_neighbors, n_pcs=self.n_pcs)
         sc.tl.umap(adata)       # umap coords are in adata.obsm['X_umap']
 
-
         return adata
 
-    # consolidate h5ads for the project.
-    
-
-class BuildFigures(object):
-    def __init__():
+    def _consolidate_adata(self, adata_dict):
         pass
+    # consolidate h5ads for the project.
 
-    
 
-    pass
-
-if __name__ =="__main__":
+if __name__ == "__main__":
 
     FORMAT = '%(asctime)s (UTC) [ %(levelname)s ] %(filename)s:%(lineno)d %(name)s.%(funcName)s(): %(message)s'
     logging.basicConfig(format=FORMAT)
@@ -290,20 +269,25 @@ if __name__ =="__main__":
                         dest='verbose',
                         help='verbose logging')
 
-            
     parser.add_argument('-c', '--config',
-                            action="store",
-                            dest='conffile',
-                            default='~/git/scqc/etc/scqc.conf',
-                            help='Config file path [~/git/scqc/etc/scqc.conf]')
+                        action="store",
+                        dest='conffile',
+                        default='~/git/scqc/etc/scqc.conf',
+                        help='Config file path [~/git/scqc/etc/scqc.conf]')
 
     parser.add_argument('-p', '--projectids',
                         metavar='projectids',
                         type=str,
                         nargs='+',
                         default=None,
-                        help='Perform tech and batch imputation on supplied projectid')
+                        help='gather stats')
 
+    parser.add_argument('-f', '--figures',
+                        metavar='projectids',
+                        type=str,
+                        nargs='+',
+                        default=None,
+                        help='build figures')
 
     args = parser.parse_args()
 
@@ -314,10 +298,10 @@ if __name__ =="__main__":
 
     if args.conffile is not None:
         cp = ConfigParser()
-        cp.read(os.path.expanduser(args.conffile)) 
+        cp.read(os.path.expanduser(args.conffile))
     else:
         cp = get_default_config()
-       
+
     cs = get_configstr(cp)
     logging.debug(f"got config: {cs}")
 
@@ -327,5 +311,5 @@ if __name__ =="__main__":
         q = GetStats(cp)
         print(args.projectids)
         for pid in args.projectids:
-        # args.projectid is a list of project ids
+            # args.projectid is a list of project ids
             q.execute(pid)
